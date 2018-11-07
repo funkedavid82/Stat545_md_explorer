790f054d289d9ce8be1ac1e9a42eacbc412cb691
funkedavid82 <30859431+funkedavid82@users.noreply.github.com>
2018-11-06 17:59:00 -0800

    2nd commit

---
+++ cm105/powers/R/square.R
@@ -0,0 +1,6 @@
+#' @rdname common_doc
+#' @title Apply a power
+#' @param x  Vector of numerics
+#' @return The vector \code{x}, with the corresponding power.
+#' @export
+square <- function(x) pow(x, 2)
