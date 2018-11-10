20313eb4702dd3496be663355b55ed5a0490ce61
funkedavid82 <30859431+funkedavid82@users.noreply.github.com>
2018-11-09 19:30:13 -0800

    Add files via upload

---
+++ cm106/powers/R/pow.R
@@ -0,0 +1,9 @@
+#' Apply a generic power
+#'
+#' Meant for internal use only.
+#'
+#' @param  x Vector to apply power to.
+#' @param  p Exponent
+#' @return  The vector x raised to the power of p
+pow <- function(x, p) x^p
+
